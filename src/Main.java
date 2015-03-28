2808c59cff5894ba093d3543e03309e8a96a1fbd
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2015-03-28 12:26:20 +0530

    commands are returning strings

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        TicTakToe game = new TicTakToe();
-        System.out.println(game.getGrid());
+        //TODO scanner is dependent on system.in
         Scanner scanner = new Scanner(System.in);
+        UserInteractor UI = new UserInteractor(scanner);
+        TicTakToe game = UI.createGame();
+        System.out.println(game.getGrid());
         ControlPannel controlPannel = new ControlPannel();
         while (true) {
-            int[] position = controlPannel.readCoordinates(scanner);
-            GameStatus status = null;
+            GameStatus status;
             try {
+                int[] position = controlPannel.readCoordinates(scanner);
                 status = controlPannel.getGameStatus(game, position);
+                System.out.println(controlPannel.gameController(game.getGrid(),status));
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
-            controlPannel.gameController(game.getGrid(),status);
         }
     }
-
 }
